908ef5d5ab5400bc70a942ede7f2b848edb779de
qcalmel <quentin.calmel@gmail.com>
2021-05-25 17:10:55 +0200

    Change link in update action with modal

--- src/components/ItemActionMenu.js
+++ src/components/ItemActionMenu.js
@@ -1,20 +1,29 @@
 import "../styles/ItemActionMenu.css"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {Link} from "react-router-dom";
+import useModal from "./useModal";
+import AddItem from "./AddItem";
+import Modal from "./Modal";
+import React from "react";
 
 const ItemActionMenu = ({itemId}) => {
+    const [isShowingUpdateItem, toggleUpdateItem] = useModal();
     return (
-        <div className="dropdown">
-            <button className="dropbtn">...</button>
-            <div className="dropdown-content">
-                <Link><FontAwesomeIcon icon="arrows-alt"/> Déplacer</Link>
-                <Link><FontAwesomeIcon icon="copy"/> Copier</Link>
-                <Link to={"/locations/" + itemId}><FontAwesomeIcon icon="edit"/> Modifier</Link>
-                <Link><FontAwesomeIcon icon="trash-alt"/> Supprimer</Link>
-                <Link><FontAwesomeIcon icon="star"/> Ajouter aux favoris</Link>
+        <>
+            <div className="action-dropdown">
+                <button className="action-dropdown-btn">...</button>
+                <div className="action-dropdown-content">
+                    <Link className="dropdown-"><FontAwesomeIcon icon="arrows-alt"/> Déplacer</Link>
+                    <Link><FontAwesomeIcon icon="copy"/> Copier</Link>
+                    <span onClick={toggleUpdateItem}><FontAwesomeIcon icon="edit"/> Modifier</span>
+                    <Link><FontAwesomeIcon icon="trash-alt"/> Supprimer</Link>
+                    <Link><FontAwesomeIcon icon="star"/> Ajouter aux favoris</Link>
+                </div>
             </div>
-        </div>
-    )
+            <Modal isShowing={isShowingUpdateItem} hide={toggleUpdateItem} title="Modifier un objet">
+                <AddItem itemId={itemId}/>
+            </Modal>
+        </>)
 }
 
 export default ItemActionMenu
