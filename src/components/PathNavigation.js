fc62627815a0d88b0a662afda30d997e74ae554a
qcalmel <quentin.calmel@gmail.com>
2021-05-25 17:06:41 +0200

    Improve PathNavigation component (color, spacing, size)

--- src/components/PathNavigation.js
+++ src/components/PathNavigation.js
@@ -1,21 +1,25 @@
 import {Link} from "react-router-dom";
+
 import ("../styles/PathNavigation.css")
 
 const PathNavigation = ({parentLocations, currentLocation}) => {
-    return(
+    return (
         <div className="path-navigation">
-        <Link to="/home/">
-            <span>Accueil</span>
-        </Link>
-    {
-        parentLocations.map((location) => (
-            <Link to={"/locations/" + location.id} key={"parent" + location.id}>
-                <span> {">"}{location.name}</span>
+            <Link to="/home/">
+                <span>Accueil</span>
             </Link>
-        ))
-    }
+            {
+                parentLocations.map((location) => (
+                    <>
+                    <span className="chevron">{" > "}</span>
+                    <Link to={"/locations/" + location.id} key={"parent" + location.id}>
+                        <span>{location.name}</span>
+                    </Link>
+                    </>
+                ))
+            }
             {currentLocation.id &&
-                <span>{">"}{currentLocation.name}</span>
+            <span>{" > "}{currentLocation.name}</span>
             }
         </div>
     )
